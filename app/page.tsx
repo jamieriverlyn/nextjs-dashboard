66e83a425650fd950701982a072c4a5eb13404e0
Jamie Gammel <jmgsquared214@gmail.com>
2024-07-04 14:10:52 -0600

    fixed page

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ export default async function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
       </div>
+      <div
+        className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+      />
       {/* <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1> */}
